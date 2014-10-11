1867060fe77cdfb969220d367810901d124d9783
alkyl1978 <alkyl1978@yandex.ru>
2014-10-11 21:18:15 +0400

    fix problem

--- spez/cserialport.h
+++ spez/cserialport.h
@@ -12,11 +12,16 @@ class CSerialPort
 {
 public:
     CSerialPort();
-    QList<QString> GiveAvaliableCom();
+    QList<QString> GiveAvaliableCom(void);
+public slots:
+
     void Slot_CSerialProt_OpenComPort(QString name);
-    void Slot_CSerialProt_CloseComPOrt();
-    void Slot_ReadData_From_Com();
+    void Slot_CSerialProt_CloseComPOrt(void);
+    void Slot_ReadData_From_Com(void);
     void Slot_Write_to_Com(QByteArray data1);
+signals:
+    void Signal_CSerialPort_SendDataToIFRNS(QByteArray data);
+private:
     QSerialPort *serial;
     int statusPRIorSON;
     int status;
