364ec05d2fca0dcafc940057d7abe24cd6571bb2
alkyl1978 <alkyl1978@yandex.ru>
2014-10-11 19:38:09 +0400

    add files

---
+++ spez/cserialport.cpp
@@ -0,0 +1,5 @@
+#include "cserialport.h"
+
+CSerialPort::CSerialPort()
+{
+}
---
+++ spez/cserialport.h
@@ -0,0 +1,10 @@
+#ifndef CSERIALPORT_H
+#define CSERIALPORT_H
+
+class CSerialPort
+{
+public:
+    CSerialPort();
+};
+
+#endif // CSERIALPORT_H
