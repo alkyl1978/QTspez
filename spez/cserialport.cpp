775a489e60d6267903715d644c79d9bd7d3048fd
alkyl1978 <alkyl1978@yandex.ru>
2014-10-11 19:54:06 +0400

    add

--- spez/cserialport.cpp
+++ spez/cserialport.cpp
@@ -2,6 +2,7 @@
 
 CSerialPort::CSerialPort()
 {
+    serial = new QSerialPort();
 }
 
 
@@ -13,4 +14,83 @@ QList<QString> CSerialPort::GiveAvaliableCom()
         List_Settings.append(info.portName());
     }
     return List_Settings;
+}
+
+void CSerialPort::Slot_CSerialProt_OpenComPort(QString name)
+{
+    //устанавливаем настройки Com
+    serial->setPortName(name);
+    serial->setBaudRate(QSerialPort::Baud115200);
+    serial->setDataBits(QSerialPort::Data8);
+    serial->setParity(QSerialPort::NoParity);
+    serial->setStopBits(QSerialPort::OneStop);
+    serial->setFlowControl(QSerialPort::NoFlowControl);
+
+if (serial->open(QIODevice::ReadWrite))
+    {
+        qDebug() << "Open is normal" ;
+        emit Signal_CSerialPort_OpenIsNormal();
+    }
+    else
+    {
+        QMessageBox::critical(this, tr("Невозможно открыть порт"),
+                              tr("Возможно порт занят"),
+                              QMessageBox::Ok);
+    }
+}
+
+void CSerialPort::Slot_CSerialProt_CloseComPOrt()
+{
+    serial->close();
+    qDebug()<< "Close is normal" ;
+}
+
+void CSerialPort::Slot_ReadData_From_Com()
+{
+    QByteArray data;
+    QApplication::processEvents();
+    if (serial->error())  qDebug()<<"Eror"<<serial->errorString()<<"   "<<serial->error();
+    data = serial->readAll();
+    if (statusPRIorSON==0)
+    {
+        if (status==0)
+        {
+            emit Signal_CSerialPort_SendDataToIFRNS(data);
+        }
+        if (status==1)
+        {
+            ;
+        }
+        if (status==2)
+        {
+            emit Signal_CSerialPort_SendDataToXModem(data);
+        }
+    }
+    else if(statusPRIorSON==1)
+    {
+        if (status==0)
+        {
+            emit Signal_CSerialPort_SendDataToPRI(data);
+        }
+        if (status==1)
+        {
+            ;
+        }
+    }
+    else if(statusPRIorSON==2)
+    {
+        if (status==0)
+        {
+            emit Signal_CSerialPort_SendDataToIFRNS_struct1(data);
+        }
+        if (status==1)
+        {
+            ;
+        }
+    }
+}
+
+void CSerialPort::Slot_Write_to_Com(QByteArray data1)
+{
+    serial->write(data1);
 }
--- spez/cserialport.h
+++ spez/cserialport.h
@@ -3,12 +3,19 @@
 
 #include <QtSerialPort/QSerialPort>
 #include <QtSerialPort/QSerialPortInfo>
+#include <QDebug>
+#include <QMessageBox>
 
 class CSerialPort
 {
 public:
     CSerialPort();
     QList<QString> GiveAvaliableCom();
+    void Slot_CSerialProt_OpenComPort(QString name);
+    void Slot_CSerialProt_CloseComPOrt();
+    void Slot_ReadData_From_Com();
+    void Slot_Write_to_Com(QByteArray data1);
+    QSerialPort *serial;
 
 };
 
