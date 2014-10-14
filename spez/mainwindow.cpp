7724ef10a3d3e8535bcb64c5268a901ad368fd99
alkyl1978 <alkyl1978@yandex.ru>
2014-10-14 11:50:22 +0400

    fix code

--- spez/mainwindow.cpp
+++ spez/mainwindow.cpp
@@ -5,17 +5,25 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
-    ini=new QSettings("setting.ini",QSettings::NativeFormat);
+    ini=new QSettings("setting.ini",QSettings::IniFormat);
     ui->setupUi(this);
     ui->comboBox->addItems(GiveAvaliableCom());
     if(ui->comboBox->count())
     {
         ui->pushButton->setEnabled(true);
     }
+    ui->vSlid_X->setValue(ini->value("Servo/X",0).toInt());
+    ui->vSlid_Y->setValue(ini->value("Servo/Y",0).toInt());
+    ui->vSlid_Z->setValue(ini->value("Servo/Z",0).toInt());
+    ini->sync();
 }
 
 MainWindow::~MainWindow()
 {
+    ini->setValue("Servo/X",ui->vSlid_X->value());
+    ini->setValue("Servo/Y",ui->vSlid_Y->value());
+    ini->setValue("Servo/Z",ui->vSlid_Z->value());
+    ini->sync();
     delete ui;
 }
 
